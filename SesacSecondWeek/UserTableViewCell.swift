1683e2adb2dc956ba84f6591b7ea8904c96c05d3
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-03 16:33:25 +0900

    커스텀 UITableViewCell 구현

---
+++ SesacSecondWeek/UserTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  UserTableViewCell.swift
+//  SesacSecondWeek
+//
+//  Created by 조성민 on 1/3/25.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+    
+    func configure(profileImage: UIImage, name: String, message: String?) {
+        profileImageView.image = profileImage
+        nameLabel.text = name
+        messageLabel.text = message
+    }
+    
+}
--- SesacSecondWeek/UserTableViewController.swift
+++ SesacSecondWeek/UserTableViewController.swift
@@ -6,11 +6,37 @@
 //
 
 import UIKit
+import Kingfisher
 
 class UserTableViewController: UITableViewController {
 
+    let name: [String] = [
+        "고래밥",
+        "칙촉",
+        "커스타드"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as? UserTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.profileImageView.kf.setImage(with: URL(string: "https://i.namu.wiki/i/izVXkClWRy9-s5DAkC_lGo3za4Zy9seGH1V6AM0qZJzsckE9eWe6-Hp-1OvJm_DkVv7BL7U0Ar7QB89ApaklkQ.webp"))
+        cell.nameLabel.text = name[indexPath.row]
+        cell.messageLabel.text = "Hello"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 
 }
