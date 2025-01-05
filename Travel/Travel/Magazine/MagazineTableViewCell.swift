1529758310961bb0afac738eeceaeba7879be32e
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-06 06:36:02 +0900

    refactor: 매거진 awakeFromNib, prepareForReuse, configure 리팩토링

--- Travel/Travel/Magazine/MagazineTableViewCell.swift
+++ Travel/Travel/Magazine/MagazineTableViewCell.swift
@@ -12,4 +12,44 @@ class MagazineTableViewCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Cell의 책임으로 옮기는 것이 좋을 것 같아요..
+        thumbnailImageView.contentMode = .scaleAspectFill
+        thumbnailImageView.layer.cornerRadius = 10
+        titleLabel.font = .systemFont(ofSize: 28, weight: .black)
+        titleLabel.numberOfLines = 2
+        subtitleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        subtitleLabel.textColor = .lightGray
+        dateLabel.font = .systemFont(ofSize: 12)
+        dateLabel.textColor = .lightGray
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+        titleLabel.text = ""
+        subtitleLabel.text = ""
+        dateLabel.text = ""
+    }
+    
+    func configure(_ content: Magazine) {
+        titleLabel.text = content.title
+        subtitleLabel.text = content.subtitle
+        thumbnailImageView.kf.setImage(with: URL(string: content.photo_image))
+        dateLabel.text = convertDateFormat(date: content.date)
+    }
+    
+    private func convertDateFormat(date: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        guard let date = dateFormatter.date(from: date) else {
+            print("잘못된 date 형식")
+            return ""
+        }
+        dateFormatter.dateFormat = "yy년 M월 d일"
+        
+        return dateFormatter.string(from: date)
+    }
 }
--- Travel/Travel/Magazine/MagazineTableViewController.swift
+++ Travel/Travel/Magazine/MagazineTableViewController.swift
@@ -30,34 +30,8 @@ class MagazineTableViewController: UITableViewController {
     ) -> UITableViewCell {
         let row = magazineInfo.magazine[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath) as! MagazineTableViewCell
-        cell.titleLabel.text = row.title
-        cell.subtitleLabel.text = row.subtitle
-        cell.thumbnailImageView.kf.setImage(with: URL(string: row.photo_image))
-        cell.dateLabel.text = convertDateFormat(date: row.date)
-        
-        // Cell의 책임으로 옮기는 것이 좋을 것 같아요..
-        cell.thumbnailImageView.contentMode = .scaleAspectFill
-        cell.thumbnailImageView.layer.cornerRadius = 10
-        cell.titleLabel.font = .systemFont(ofSize: 28, weight: .black)
-        cell.titleLabel.numberOfLines = 2
-        cell.subtitleLabel.font = .systemFont(ofSize: 16, weight: .bold)
-        cell.subtitleLabel.textColor = .lightGray
-        cell.dateLabel.font = .systemFont(ofSize: 12)
-        cell.dateLabel.textColor = .lightGray
+        cell.configure(row)
         
         return cell
     }
-    
-    private func convertDateFormat(date: String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyMMdd"
-        guard let date = dateFormatter.date(from: date) else {
-            print("잘못된 date 형식")
-            return ""
-        }
-        dateFormatter.dateFormat = "yy년 M월 d일"
-        
-        return dateFormatter.string(from: date)
-    }
-    
 }
