d75b759fc2680702397cf0223c0d46aaf8d387cd
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-06 06:44:30 +0900

    refactor: 도시 awakeFromNib, prepareFoReuse, configure 리팩토링

--- Travel/Travel/City/CityTableViewCell.swift
+++ Travel/Travel/City/CityTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  CityTableViewCell.swift
+//  CityTableViewswift
 //  Travel
 //
 //  Created by 조성민 on 1/4/25.
@@ -14,4 +14,51 @@ class CityTableViewCell: UITableViewCell {
     @IBOutlet var informationLabel: UILabel!
     @IBOutlet var thumbnailImageView: UIImageView!
     @IBOutlet var likeButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        titleLabel.numberOfLines = 0
+        titleLabel.font = .systemFont(ofSize: 16, weight: .black)
+        descriptionLabel.textColor = .darkGray
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.font = .systemFont(ofSize: 14)
+        informationLabel.font = .systemFont(ofSize: 12)
+        informationLabel.textColor = .lightGray
+        informationLabel.numberOfLines = 0
+        thumbnailImageView.contentMode = .scaleToFill
+        thumbnailImageView.layer.cornerRadius = 10
+        likeButton.tintColor = .systemPink
+        for idx in 0..<5 {
+            starImageViewList[idx].image = UIImage(systemName: "star.fill")
+            starImageViewList[idx].tintColor = .systemGray5
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = ""
+        descriptionLabel.text = ""
+        for idx in 0..<5 {
+            starImageViewList[idx].tintColor = .systemGray5
+        }
+        informationLabel.text = ""
+        thumbnailImageView.image = UIImage(systemName: "photo")
+        likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+    }
+    
+    func configure(_ content: Travel) {
+        titleLabel.text = content.title
+        descriptionLabel.text = content.description
+        let grade = Int(content.grade ?? 0)
+        for idx in 0..<5 {
+            starImageViewList[idx].tintColor = grade > idx ? .systemYellow : .systemGray5
+        }
+        informationLabel.text = "(\((content.rateCount ?? 0).changeToDecimalFormat())) · 저장 \((content.save ?? 0).changeToDecimalFormat())"
+        if let imageURLString = content.travel_image {
+            thumbnailImageView.kf.setImage(with: URL(string: imageURLString))
+        } else {
+            thumbnailImageView.image = UIImage(systemName: "photo")
+        }
+        likeButton.setImage(UIImage(systemName: content.like ?? false ? "heart.fill" : "heart"), for: .normal)
+    }
 }
--- Travel/Travel/City/CityTableViewController.swift
+++ Travel/Travel/City/CityTableViewController.swift
@@ -29,36 +29,7 @@ class CityTableViewController: UITableViewController {
     ) -> UITableViewCell {
         let row = travelInfo.travel[indexPath.row]
         if !row.ad, let cell = tableView.dequeueReusableCell(withIdentifier: "CityTableViewCell", for: indexPath) as? CityTableViewCell {
-            cell.titleLabel.text = row.title
-            cell.titleLabel.numberOfLines = 0
-            cell.titleLabel.font = .systemFont(ofSize: 16, weight: .black)
-            
-            cell.descriptionLabel.text = row.description
-            cell.descriptionLabel.textColor = .darkGray
-            cell.descriptionLabel.numberOfLines = 0
-            cell.descriptionLabel.font = .systemFont(ofSize: 14)
-            
-            let grade = Int(row.grade ?? 0)
-            for idx in 0..<5 {
-                cell.starImageViewList[idx].image = UIImage(systemName: "star.fill")
-                cell.starImageViewList[idx].tintColor = grade > idx ? .systemYellow : .systemGray5
-            }
-            
-            cell.informationLabel.text = "(\((row.rateCount ?? 0).changeToDecimalFormat())) · 저장 \((row.save ?? 0).changeToDecimalFormat())"
-            cell.informationLabel.font = .systemFont(ofSize: 12)
-            cell.informationLabel.textColor = .lightGray
-            cell.informationLabel.numberOfLines = 0
-            
-            cell.thumbnailImageView.contentMode = .scaleToFill
-            cell.thumbnailImageView.layer.cornerRadius = 10
-            if let imageURLString = row.travel_image {
-                cell.thumbnailImageView.kf.setImage(with: URL(string: imageURLString))
-            } else {
-                cell.thumbnailImageView.image = UIImage(systemName: "photo")
-            }
-            
-            cell.likeButton.setImage(UIImage(systemName: row.like ?? false ? "heart.fill" : "heart"), for: .normal)
-            cell.likeButton.tintColor = .systemPink
+            cell.configure(row)
             cell.likeButton.tag = indexPath.row
             cell.likeButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
             
