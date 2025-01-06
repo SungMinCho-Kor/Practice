a018c71036fefef816b69bddda40594ce1005425
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-01-06 21:30:36 +0900

    feat: 도시 검색 구현

--- Travel/Travel/SearchCity/CityTableViewController.swift
+++ Travel/Travel/SearchCity/CityTableViewController.swift
@@ -13,11 +13,38 @@ class CityTableViewController: UITableViewController {
     @IBOutlet private var categorySegmentedControl: UISegmentedControl!
     
     private let cityInfo = CityInfo()
+    private var allCityInfo: [City] {
+        cityInfo.city.filter({
+            if let textFieldText = searchTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased().replacingOccurrences(of: " ", with: ""), !textFieldText.isEmpty {
+                return $0.city_english_name.lowercased().contains(textFieldText) ||
+                $0.city_name.lowercased().contains(textFieldText) ||
+                $0.city_explain.lowercased().contains(textFieldText)
+            } else {
+                return true
+            }
+        })
+    }
     private var internalCityInfo: [City] {
-        cityInfo.city.filter({ $0.domestic_travel })
+        cityInfo.city.filter({
+            if let textFieldText = searchTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased().replacingOccurrences(of: " ", with: ""), !textFieldText.isEmpty {
+                return ($0.city_english_name.lowercased().contains(textFieldText) ||
+                $0.city_name.lowercased().contains(textFieldText) ||
+                $0.city_explain.lowercased().contains(textFieldText)) && $0.domestic_travel
+            } else {
+                return $0.domestic_travel
+            }
+        })
     }
     private var domesticCityInfo: [City] {
-        cityInfo.city.filter({ !$0.domestic_travel })
+        cityInfo.city.filter({
+            if let textFieldText = searchTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).lowercased().replacingOccurrences(of: " ", with: ""), !textFieldText.isEmpty {
+                return ($0.city_english_name.lowercased().contains(textFieldText) ||
+                $0.city_name.lowercased().contains(textFieldText) ||
+                $0.city_explain.lowercased().contains(textFieldText)) && !$0.domestic_travel
+            } else {
+                return !$0.domestic_travel
+            }
+        })
     }
     
     override func viewDidLoad() {
@@ -47,6 +74,17 @@ class CityTableViewController: UITableViewController {
     
     private func searchTextFieldDesign() {
         searchTextField.borderStyle = .none
+        searchTextField.placeholder = "도시를 검색해보세요"
+        searchTextField.addTarget(
+            self,
+            action: #selector(searchTextFieldChanged),
+            for: .editingChanged
+        )
+        searchTextField.addTarget(
+            self,
+            action: #selector(searchTextFieldReturnTapped),
+            for: .editingDidEndOnExit
+        )
     }
     
     private func categorySegmentedControlDesign() {
@@ -65,7 +103,7 @@ class CityTableViewController: UITableViewController {
         numberOfRowsInSection section: Int
     ) -> Int {
         if categorySegmentedControl.selectedSegmentIndex == 0 {
-            return cityInfo.city.count
+            return allCityInfo.count
         } else if categorySegmentedControl.selectedSegmentIndex == 1 {
             return internalCityInfo.count
         } else if categorySegmentedControl.selectedSegmentIndex == 2 {
@@ -88,7 +126,7 @@ class CityTableViewController: UITableViewController {
             return UITableViewCell()
         }
         if categorySegmentedControl.selectedSegmentIndex == 0 {
-            cell.configure(cityInfo.city[indexPath.row])
+            cell.configure(allCityInfo[indexPath.row])
         } else if categorySegmentedControl.selectedSegmentIndex == 1 {
             cell.configure(internalCityInfo[indexPath.row])
         } else if categorySegmentedControl.selectedSegmentIndex == 2 {
@@ -100,9 +138,20 @@ class CityTableViewController: UITableViewController {
         return cell
     }
     
-    @objc private func categorySegmentedControlChanged(_ sender: UISegmentedControl) {
-        searchTextField.text = ""
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         view.endEditing(true)
+    }
+    
+    @objc private func categorySegmentedControlChanged(_ sender: UISegmentedControl) {
+        view.endEditing(true)
+        tableView.reloadData()
+    }
+    
+    @objc private func searchTextFieldChanged(_ sender: UITextField) {
+        tableView.reloadData()
+    }
+    
+    @objc private func searchTextFieldReturnTapped(_ sender: UITextField) {
         tableView.reloadData()
     }
 }
