8d540a952d2eed9fc88b11bf18ce0adf32d4ebd4
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-03 15:36:15 +0900

    build: Weather 프로젝트 생성

---
+++ Weather/Weather/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Weather
+//
+//  Created by 조성민 on 2/3/25.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by 조성민 on 2/3/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
