27c5cb350633fb3fbe721086b048534b00e9c644
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-04 15:12:19 +0900

    feat: Observable

---
+++ SeventhWeek/SeventhWeek/Basic/Observable.swift
@@ -0,0 +1,26 @@
+//
+//  Observable.swift
+//  SeventhWeek
+//
+//  Created by 조성민 on 2/4/25.
+//
+
+class Observable<T> {
+    
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+     
+    func bind(closure: @escaping (T) -> Void) {
+        closure(value)
+        self.closure = closure
+    }
+}
