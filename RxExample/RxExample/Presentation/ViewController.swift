c412d021e8de64341e1b3563dcba1ab39d9e5110
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-18 15:38:38 +0900

    build: Rx 프로젝트 생성 및 세팅

---
+++ RxExample/RxExample/Presentation/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  RxExample
+//
+//  Created by 조성민 on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
