6a53f2429ff721f5667799c09103f98e9ab568c3
SungMinCho-Kor <rhxoeh0011@gmail.com>
2025-02-24 15:25:29 +0900

    feat: Single 적용

--- SeSACRxThreads/SeSACRxThreads/Search/NetworkManager.swift
+++ SeSACRxThreads/SeSACRxThreads/Search/NetworkManager.swift
@@ -58,12 +58,56 @@ final class NetworkManager {
         .resume()
     }
     
-    func callBoxOffice(date: String) -> Observable<Movie> {
-        return Observable<Movie>.create { observer in
+//    func callBoxOffice(date: String) -> Observable<Movie> {
+//        return Observable<Movie>.create { observer in
+//            let urlString =  "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=6f880d27184cbe92e28d4970282cec8e&targetDt=\(date)"
+//            
+//            guard let url = URL(string: urlString ) else {
+//                observer.onError(APIError.invalidURL)
+//                return Disposables.create {
+//                    print("URL ERROR!")
+//                }
+//            }
+//            
+//            URLSession.shared.dataTask(with: url) { data, response, error in
+//                if let error {
+//                    observer.onError(APIError.unknownResponse)
+//                    return
+//                }
+//                
+//                guard let response = response as? HTTPURLResponse,
+//                      (200...299).contains(response.statusCode) else {
+//                    observer.onError(APIError.statusError)
+//                    return
+//                }
+//                
+//                if let data {
+//                    do {
+//                        let result = try JSONDecoder().decode(Movie.self, from: data)
+//                        observer.onNext(result)
+//                    } catch {
+//                        observer.onError(APIError.unknownResponse)
+//                    }
+//                } else {
+//                    observer.onError(APIError.unknownResponse)
+//                }
+//            }
+//            .resume()
+//            
+//            return Disposables.create {
+//                print("끝!")
+//            }
+//        }
+//        
+//    }
+    
+    func callBoxOffice(date: String) -> Single<Movie> {
+        return Single<Movie>.create { observer in
+            observer(.failure(APIError.unknownResponse))
             let urlString =  "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=6f880d27184cbe92e28d4970282cec8e&targetDt=\(date)"
             
             guard let url = URL(string: urlString ) else {
-                observer.onError(APIError.invalidURL)
+                observer(.failure(APIError.invalidURL))
                 return Disposables.create {
                     print("URL ERROR!")
                 }
@@ -71,25 +115,25 @@ final class NetworkManager {
             
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let error {
-                    observer.onError(APIError.unknownResponse)
+                    observer(.failure(APIError.unknownResponse))
                     return
                 }
                 
                 guard let response = response as? HTTPURLResponse,
                       (200...299).contains(response.statusCode) else {
-                    observer.onError(APIError.statusError)
+                    observer(.failure(APIError.statusError))
                     return
                 }
                 
                 if let data {
                     do {
                         let result = try JSONDecoder().decode(Movie.self, from: data)
-                        observer.onNext(result)
+                        observer(.success(result))
                     } catch {
-                        observer.onError(APIError.unknownResponse)
+                        observer(.failure(APIError.unknownResponse))
                     }
                 } else {
-                    observer.onError(APIError.unknownResponse)
+                    observer(.failure(APIError.unknownResponse))
                 }
             }
             .resume()
